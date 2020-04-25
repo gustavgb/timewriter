5e22b3fc1f8ba6709f7eef6f1584c4b7fc0da80d
Gustav Burchardt <gustavburchardt@gmail.com>
2020-04-26 00:19:46 +0200

    Implement basic ui

--- src/Console.js
+++ src/Console.js
@@ -1,5 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import styled from 'styled-components'
+import Textarea from 'react-textarea-autosize'
+
+const History = styled.div`
+  align-items: end;
+`
 
 const Root = styled.div`
   background-color: #333;
@@ -9,7 +14,13 @@ const Root = styled.div`
   max-width: calc(100vw - 30vh);
   height: 70vh;
 
-  & > * {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+
+  & > ${History},
+  & textarea {
     color: white;
     font-size: 2.5rem;
     line-height: 3.5rem;
@@ -21,41 +32,62 @@ const Root = styled.div`
   }
 `
 
-const History = styled.div`
-`
-
-const Command = styled.input`
-  height: 3.5rem;
-  background: transparent;
-  border: 0;
-  outline: none;
-  cursor: default;
+const Command = styled.div.attrs(props => ({
+  style: {
+    height: props.height + 'px'
+  }
+}))`
+  & textarea {
+    background: transparent;
+    border: 0;
+    outline: none;
+    width: 100%;
+    padding: 0;
+    resize: none;
+  }
 `
 
 const Console = () => {
   const [command, setCommand] = useState('')
-  const inputRef = React.createRef()
+  const [clickPosition, setClickPosition] = useState(null)
+  const [inputHeight, setInputHeight] = useState(0)
+  const inputRef = useRef(null)
 
-  const onClickRoot = useCallback(() => {
+  const focusInput = useCallback(() => {
     if (inputRef.current) {
       inputRef.current.focus()
     }
-  }, [inputRef])
+  }, [])
+
+  const registerMousePosition = useCallback((e) => {
+    setClickPosition(`${e.clientX}x${e.clientY}`)
+  }, [])
+
+  const determineClick = useCallback((e) => {
+    if (`${e.clientX}x${e.clientY}` === clickPosition) {
+      focusInput()
+    }
+  }, [focusInput, clickPosition])
 
   useEffect(() => {
-    onClickRoot()
-  }, [onClickRoot])
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
 
   return (
-    <Root>
-      <Command
-        value={command}
-        onChange={(e) => setCommand(e.target.value)}
-        ref={inputRef}
-      />
+    <Root onMouseDown={registerMousePosition} onMouseUp={determineClick}>
       <History>
         History line
       </History>
+      <Command height={inputHeight}>
+        <Textarea
+          inputRef={(el) => { inputRef.current = el }}
+          value={`${command}`}
+          onChange={(e) => setCommand(e.target.value)}
+          onHeightChange={setInputHeight}
+        />
+      </Command>
     </Root>
   )
 }
